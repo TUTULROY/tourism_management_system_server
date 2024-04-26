1a392550dee1ba3bd1609dc1a2a7718c43139408
TUTULROY <tutulroyjoy@gmail.com>
2024-04-26 19:54:49 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// middleware
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get('/', (req, res) =>{
+    res.send('tourism management server is running')
+})
+
+app.listen(port, () =>{
+    console.log(`Tourism management server is running on port: ${port}`)
+})
